f491cd2e0cf8a90a46ec5f5524d0e2654bf3c6aa
zou_chao824 <zou_chao824@163.com>
2023-10-08 12:15:20 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Rdmi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Rdmi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
